84edfcf8f45581b0824f2419e7a99d5de84f2b2d
Michael Smith <me@murray-mint.co.uk>
2015-10-27 23:55:20 +0000

    Add a readme file Define volumes in Dockerfile Update run script to pull the image from Docker Hub.

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 . config.sh
-docker run --name steamcache -v ${STEAMCACHE_DATA}:/var/www -v ${STEAMCACHE_LOGS}:/var/log/nginx -d -p ${STEAMCACHE_IP}:80:80 steamcache
+docker run --name steamcache -v ${STEAMCACHE_DATA}:/var/www -v ${STEAMCACHE_LOGS}:/var/log/nginx -d -p ${STEAMCACHE_IP}:80:80 murraymint/steamcache:latest
