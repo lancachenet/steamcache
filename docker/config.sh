f447c85d2e36e89ea4df5a924382f0b39ef8ba5d
Jason Rivers <jason@jasonrivers.co.uk>
2015-12-19 16:01:06 +0000

    use HOST_IP for the host IP address
    Required for the address for DNS records

--- docker/config.sh
+++ docker/config.sh
@@ -1,5 +1,7 @@
 # The IP to bind the steamcache container to
 STEAMCACHE_IP=0.0.0.0
+# The IP of the HOST Machine
+HOST_IP=192.168.0.5
 # The dir containing the cache data
 STEAMCACHE_DATA=/var/www
 # The dir to write steamcache logs to
--- docker/run.sh
+++ docker/run.sh
@@ -6,6 +6,6 @@ docker run --name steamcache			\
 	-v ${STEAMCACHE_LOGS}:/var/log/nginx	\
 	-p ${STEAMCACHE_IP}:80:80 		\
 	-p ${STEAMCACHE_IP}:53:53/udp		\
-	-e HOSTIP=${STEAMCACHE_IP}		\
+	-e HOSTIP=${HOST_IP}		\
 	murraymint/steamcache:latest
 
