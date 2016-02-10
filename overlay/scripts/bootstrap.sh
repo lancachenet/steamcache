f58dbbfb4f31525b7bdc08f0aa27072b273a56d2
Jason Rivers <jason@jasonrivers.co.uk>
2016-02-10 01:19:20 +0000

    Fix bootstrap to use HOST_IP as per the written documentation

--- overlay/scripts/bootstrap.sh
+++ overlay/scripts/bootstrap.sh
@@ -5,8 +5,8 @@
 cp /etc/bind/steamcache/template.db.content_.steampowered.com /etc/bind/steamcache/db.content_.steampowered.com
 cp /etc/bind/steamcache/template.db.cs.steampowered.com /etc/bind/steamcache/db.cs.steampowered.com
 
-sed -i -e "s%{{ steamcache_ip }}%$HOSTIP%g" /etc/bind/steamcache/db.content_.steampowered.com
-sed -i -e "s%{{ steamcache_ip }}%$HOSTIP%g" /etc/bind/steamcache/db.cs.steampowered.com
+sed -i -e "s%{{ steamcache_ip }}%$HOST_IP%g" /etc/bind/steamcache/db.content_.steampowered.com
+sed -i -e "s%{{ steamcache_ip }}%$HOST_IP%g" /etc/bind/steamcache/db.cs.steampowered.com
 
 service bind9 start
 service nginx restart
