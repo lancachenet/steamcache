07ec548a88d1830e58544a0bddc031ac9214bd61
Jason Rivers <jason@jasonrivers.co.uk>
2018-06-28 12:09:53 +0100

    Fix permissions at startup
    fixes #32
    fixes #29

--- overlay/scripts/bootstrap.sh
+++ overlay/scripts/bootstrap.sh
@@ -3,6 +3,10 @@ set -e
 
 . /scripts/config.sh
 
+echo "Checking permissions (This may take a long time if the permissions are incorrect on large caches)..."
+find /data \! -user ${WEBUSER} -exec chown ${WEBUSER}:${WEBUSER} '{}' +
+echo "Done. Starting caching server."
+
 /usr/sbin/nginx -t
 
 /usr/sbin/nginx -g "daemon off;"
